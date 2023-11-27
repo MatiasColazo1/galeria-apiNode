9399ff90383010cdb6e47741932e118e3e41527f
MatiasColazo1 <maty_colazo@hotmail.com>
2023-11-27 17:35:53 -0300

    crear fotos

--- dist/controllers/fotos.controllers.js
+++ dist/controllers/fotos.controllers.js
@@ -1,14 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.crearFotos = exports.getFotos = void 0;
+const Fotos_1 = __importDefault(require("../models/Fotos"));
 function getFotos(req, res) {
 }
 exports.getFotos = getFotos;
-function crearFotos(req, res) {
-    console.log('Guardando foto');
-    console.log(req.body);
+async function crearFotos(req, res) {
+    const { titulo, descripcion } = req.body;
+    const newFoto = {
+        titulo: titulo,
+        descripcion: descripcion,
+        imagenPath: req.file?.path
+    };
+    const foto = new Fotos_1.default(newFoto);
+    await foto.save();
     return res.json({
-        message: 'Foto guardada'
+        message: 'Foto guardada',
+        foto
     });
 }
 exports.crearFotos = crearFotos;
--- src/controllers/fotos.controllers.ts
+++ src/controllers/fotos.controllers.ts
@@ -1,15 +1,23 @@
 import { Request, Response } from "express"
 
+import Fotos from "../models/Fotos";
+
 export function getFotos(req: Request, res: Response){
 
 }
 
-export function crearFotos(req: Request, res: Response){
+export async function crearFotos(req: Request, res: Response){
 
-    console.log('Guardando foto')
-    console.log(req.body)
-
+    const { titulo, descripcion } = req.body;
+    const newFoto = {
+        titulo: titulo,
+        descripcion: descripcion,
+        imagenPath: req.file?.path
+    };
+    const foto = new Fotos(newFoto);
+    await foto.save();
     return res.json({
-        message: 'Foto guardada'
+        message: 'Foto guardada',
+        foto
     })
 }
